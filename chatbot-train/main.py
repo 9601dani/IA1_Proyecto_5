20eca652b02992a37f1ad2e75b23d2c04353f0d2
Leví Hernández <levihernandez2501@gmail.com>
2024-12-07 20:59:37 -0600

    Add s2s model, tokenizer metadata, datasets, main.py and train.py

---
+++ chatbot-train/main.py
@@ -0,0 +1,81 @@
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+
+# Cargar el modelo completo
+inf_model = tf.keras.models.load_model('s2s_model.keras')
+
+# Cargar el tokenizer y los metadatos
+with open('tokenizer.pkl', 'rb') as file:
+    metadata = pickle.load(file)
+
+tokenizer = metadata["tokenizer"]
+maxlen_questions = metadata["maxlen_questions"]
+maxlen_answers = metadata["maxlen_answers"]
+
+# Crear el modelo del codificador
+encoder_inputs = inf_model.input[0]  # Primera entrada del modelo
+encoder_outputs, state_h_enc, state_c_enc = inf_model.layers[4].output
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = tf.keras.models.Model(encoder_inputs, encoder_states)
+
+# Crear el modelo del decodificador
+decoder_inputs = inf_model.input[1]  # Segunda entrada del modelo
+decoder_state_input_h = tf.keras.layers.Input(shape=(200,), name="input_3")  # Tamaño de la capa LSTM
+decoder_state_input_c = tf.keras.layers.Input(shape=(200,), name="input_4")
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+decoder_embedding = inf_model.layers[3](decoder_inputs)
+decoder_lstm = inf_model.layers[5]
+decoder_outputs, state_h, state_c = decoder_lstm(
+    decoder_embedding, initial_state=decoder_states_inputs
+)
+decoder_dense = inf_model.layers[6]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_states = [state_h, state_c]
+
+decoder_model = tf.keras.models.Model(
+    [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+)
+
+def preprocess_input(input_sentence, tokenizer, maxlen_questions):
+    tokens = input_sentence.lower().split()
+    tokens_list = []
+    for word in tokens:
+        tokens_list.append(tokenizer.word_index[word])
+    return pad_sequences([tokens_list], maxlen=maxlen_questions, padding='post')
+
+tests = ['Hello', 'Are you a bot', 'What is your name', 'Do know any jokes', 'what is AI']
+
+for test_sentence in tests:
+    states_values = encoder_model.predict(preprocess_input(test_sentence, tokenizer, maxlen_questions))
+
+    empty_target_seq = np.zeros((1, 1))
+    empty_target_seq[0, 0] = tokenizer.word_index['start']
+    stop_condition = False
+    decoded_translation = ''
+
+    while not stop_condition:
+        # Predecir la siguiente palabra
+        dec_outputs, h, c = decoder_model.predict([empty_target_seq] + states_values)
+        sampled_word_index = np.argmax(dec_outputs[0, -1, :])
+        sampled_word = None
+
+        for word, index in tokenizer.word_index.items():
+            if sampled_word_index == index:
+                decoded_translation += f' {word}'
+                sampled_word = word
+
+        if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
+            stop_condition = True
+
+        # Actualizar la secuencia de entrada y los estados
+        empty_target_seq = np.zeros((1, 1))
+        empty_target_seq[0, 0] = sampled_word_index
+        states_values = [h, c]
+
+    print(f'Human: {test_sentence}')
+    print(f'Bot: {decoded_translation.split(" end")[0]}')
+    print('-' * 25)
---
+++ chatbot-train/train.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import pickle
+from tensorflow.keras import layers, activations, models, utils
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
+import os
+import yaml
+
+# Print filenames in the input directory
+# for dirname, _, filenames in os.walk('./kaggle/input'):
+#     for filename in filenames:
+#         print(os.path.join(dirname, filename))
+
+# Directory path for YAML files
+dir_path = './kaggle/input/'
+files_list = os.listdir(dir_path + os.sep)
+
+# Parse conversations from YAML files
+questions, answers = [], []
+
+for filepath in files_list:
+    with open(dir_path + os.sep + filepath, 'rb') as file_:
+        docs = yaml.safe_load(file_)
+        conversations = docs['conversations']
+        for con in conversations:
+            if len(con) > 2:
+                questions.append(con[0])
+                replies = con[1:]
+                ans = ''
+                for rep in replies:
+                    ans += ' ' + rep
+                answers.append(ans)
+            elif len(con) > 1:
+                questions.append(con[0])
+                answers.append(con[1])
+
+# Preprocessing answers
+answers_with_tags = []
+for i in range(len(answers)):
+    if type(answers[i]) == str:
+        answers_with_tags.append(answers[i])
+    else:
+        questions.pop(i)
+
+answers = []
+for i in range(len(answers_with_tags)):
+    answers.append('<START> ' + answers_with_tags[i] + ' <END>')
+
+# Tokenization
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(questions + answers)
+VOCAB_SIZE = len(tokenizer.word_index) + 1
+
+from gensim.models import Word2Vec
+
+vocab = []
+for word in tokenizer.word_index:
+    vocab.append(word)
+
+def tokenize(sentences):
+    tokens_list = []
+    vocabulary = []
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+        tokens = sentence.split()
+        vocabulary += tokens
+        tokens_list.append(tokens)
+    return tokens_list , vocabulary
+
+tokenized_questions = tokenizer.texts_to_sequences(questions)
+maxlen_questions = max([len(x) for x in tokenized_questions])
+padded_questions = pad_sequences(tokenized_questions , maxlen=maxlen_questions , padding='post')
+encoder_input_data = np.array(padded_questions)
+
+print(encoder_input_data.shape)
+
+tokenized_answers = tokenizer.texts_to_sequences(answers)
+maxlen_answers = max([len(x) for x in tokenized_answers])
+padded_answers = pad_sequences(tokenized_answers , maxlen=maxlen_answers , padding='post')
+decoder_input_data = np.array(padded_answers)
+
+print(decoder_input_data.shape)
+
+tokenized_answers = tokenizer.texts_to_sequences(answers)
+for i in range(len(tokenized_answers)) :
+    tokenized_answers[i] = tokenized_answers[i][1:]
+padded_answers = pad_sequences(tokenized_answers , maxlen=maxlen_answers , padding='post')
+onehot_answers = utils.to_categorical(padded_answers , VOCAB_SIZE)
+decoder_output_data = np.array(onehot_answers)
+
+print(decoder_output_data.shape)
+
+encoder_inputs = tf.keras.layers.Input(shape=(maxlen_questions ,))
+encoder_embedding = tf.keras.layers.Embedding(VOCAB_SIZE, 200 , mask_zero=True) (encoder_inputs)
+encoder_outputs , state_h , state_c = tf.keras.layers.LSTM(200 , return_state=True)(encoder_embedding)
+encoder_states = [ state_h , state_c ]
+
+decoder_inputs = tf.keras.layers.Input(shape=(maxlen_answers , ))
+decoder_embedding = tf.keras.layers.Embedding(VOCAB_SIZE, 200 , mask_zero=True) (decoder_inputs)
+decoder_lstm = tf.keras.layers.LSTM(200 , return_state=True , return_sequences=True)
+decoder_outputs , _ , _ = decoder_lstm (decoder_embedding , initial_state=encoder_states)
+decoder_dense = tf.keras.layers.Dense(VOCAB_SIZE , activation=tf.keras.activations.softmax) 
+output = decoder_dense (decoder_outputs)
+
+model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output)
+model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='categorical_crossentropy')
+
+model.summary()
+
+model.fit([encoder_input_data , decoder_input_data], decoder_output_data, batch_size=32, epochs=150)
+
+
+model.save("s2s_model.keras")
+
+metadata = {
+    "tokenizer": tokenizer,
+    "maxlen_questions": maxlen_questions,
+    "maxlen_answers": maxlen_answers
+}
+
+with open('tokenizer.pkl', 'wb') as file:
+    pickle.dump(metadata, file)
+    
+print("Modelo guardado como 's2s_model.keras'.")
